383e169fc7e7ab3dc9d11e03ab2c168c0218a948
bnorthan <bnorthan@gmail.com>
2024-09-17 07:51:26 -0400

    Add member function to set model directly

--- src/napari_easy_augment_batch_dl/base_model.py
+++ src/napari_easy_augment_batch_dl/base_model.py
@@ -5,6 +5,9 @@ class BaseModel:
         self.num_classes = num_classes
         self.patch_path = patch_path
 
+    def set_model(self, model):
+        self.model = model
+
     def train(self, num_epochs, updater=None):
         pass
 
