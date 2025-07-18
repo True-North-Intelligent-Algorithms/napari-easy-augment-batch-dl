b48609a382d4cf7a008352ef0031d5fffc68edc2
bnorthan <bnorthan@gmail.com>
2025-07-17 20:37:48 -0400

    First commit of Pixi dependency management config files

---
+++ pixi/microsam_cellpose3/startup.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+print("Startup running...")
+
+import napari
+print("Napari version:", napari.__version__)
+
+import torch
+
+# Print PyTorch version
+print("PyTorch version:", torch.__version__)
+
+# Check if CUDA (GPU) is available
+print("CUDA available:", torch.cuda.is_available())
+
+# If available, print the name of the GPU
+if torch.cuda.is_available():
+    print("GPU name:", torch.cuda.get_device_name(0))
+    print("Current device:", torch.cuda.current_device())
+else:
+    print("Running on CPU")
+
+import cellpose
+print("Cellpose version:", cellpose.version)
+
+viewer = napari.Viewer()
+napari.run()
